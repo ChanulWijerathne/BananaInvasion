5b3622a70c571d725cd90a26cd7fcd2ea2c8a38b
Chanu Wijerathne <chanulwijerathna@gmail.com>
2026-03-12 11:48:06 +0530

    Characters update

--- src/main/java/bananainvasion/Enemy.java
+++ src/main/java/bananainvasion/Enemy.java
@@ -1,24 +1,35 @@
 package bananainvasion;
 
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+
+import java.util.Objects;
 
 public class Enemy extends Rectangle {
 
     private final double speed;
 
     public Enemy(double x, double y, double speed) {
-        super(35, 55);
+        super(120, 120);
         this.speed = speed;
 
         setLayoutX(x);
         setLayoutY(y);
 
-        setFill(Color.CRIMSON);
-        setStroke(Color.DARKRED);
+        try {
+            Image bananaImage = new Image(
+                    Objects.requireNonNull(getClass().getResourceAsStream("/images/banana.png"))
+            );
+            setFill(new ImagePattern(bananaImage));
+        } catch (Exception e) {
+            setFill(Color.CRIMSON);
+            System.out.println("Banana image not found: " + e.getMessage());
+        }
+
         setArcWidth(20);
         setArcHeight(20);
-        setRotate(20);
     }
 
     public void update() {
